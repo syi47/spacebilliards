df82b1a984c980cc7ba8db704163b44c3027c1a6
tjohns20 <tjohns20@2d35bbc6-a434-0410-ba66-8ba39d4a960b>
2007-07-07 10:45:56 +0000

    Initial import - 2nd attempt

---
+++ trunk/src/Game.cpp
@@ -0,0 +1,233 @@
+#include "Game.h"
+#include "fmod.h"
+#include "ObjectFactory.h"
+
+#pragma comment(lib, "irrlicht.lib")
+#pragma comment(lib, "fmodvc.lib")
+
+
+typedef std::vector<MovingObject*>::iterator Object_it;
+
+
+Game::Game(void)
+: m_PlayArea(130, 0.03f)
+{
+	//Initialise FMOD
+	LOG_INFO("Initialising FMOD");
+	FSOUND_Init(44100, 32, 0);
+}
+
+Game::~Game(void)
+{
+}
+
+void Game::action()
+{
+	//Set the window caption
+	Irrlicht::getDevice()->setWindowCaption(L"Asteroids Demo");
+
+	//Point irrlicht at the data folder for resources
+	Irrlicht::getDevice()->getFileSystem()->changeWorkingDirectoryTo("data");
+
+	//seed rand() with current time
+	srand(Irrlicht::getDevice()->getTimer()->getRealTime() );
+
+	//Set the player object as the device's EventReceiver
+	Irrlicht::getDevice()->setEventReceiver(&m_Player);
+
+	loadScene();
+
+	//run the game
+	LOG_INFO("Entering game loop...");
+	while(Irrlicht::getDevice()->run() )
+	{
+		//Update the player
+		m_Player.Update();
+
+		//Let the game world affect each object within it
+		for (Object_it object = m_Objects.begin(); object != m_Objects.end(); ++object)
+		{
+			m_PlayArea.actOn(*object);
+		}
+
+		checkCollisions();
+
+		//check the objects for deletion
+		Object_it object_i = m_Objects.begin();
+		while (object_i != m_Objects.end() )
+		{
+			Object_it object = object_i++;
+
+			if ( (*object)->getSceneNode() == 0 )
+			{
+				//object has been cleaned up and is awaiting deletion
+				delete (*object);
+				object_i = m_Objects.erase(object);
+			}
+		}
+
+		if (Irrlicht::getDevice()->isWindowActive()
+			&& Irrlicht::getDevice()->getVideoDriver() != 0
+			&& Irrlicht::getDevice()->getSceneManager() != 0)
+		{
+			Irrlicht::getDevice()->getVideoDriver()->beginScene(true, true, irr::video::SColor(255, 0, 0, 0) );
+			Irrlicht::getDevice()->getSceneManager()->drawAll();
+			Irrlicht::getDevice()->getVideoDriver()->endScene();
+		}
+
+		//Check for all asteroids sunk
+		bool asteroidsleft = false;
+		for (Object_it object = m_Objects.begin(); object != m_Objects.end(); ++object)
+		{
+			if ( (*object)->getType() == OT_ASTEROID)
+			{
+				asteroidsleft = true;
+			}
+		}
+		if (!asteroidsleft)
+		{
+			releaseScene();
+			loadScene();
+		}
+
+	}
+	LOG_INFO("...Exiting game loop");
+
+	releaseScene();
+
+	//Remove the player as the Event Receiver before deletion
+	Irrlicht::getDevice()->setEventReceiver(0);
+
+	//Clean up FMOD
+	LOG_INFO("Closing FMOD");
+	FSOUND_Close();
+}
+
+void Game::checkCollisions()
+{
+	//Check for collisions between objects
+	if (m_Objects.empty() )
+		return;
+
+	for (Object_it CurrentObject = m_Objects.begin();
+			CurrentObject != m_Objects.end()-1; ++CurrentObject)
+	{
+		for (Object_it OtherObject = CurrentObject+1;
+				OtherObject != m_Objects.end(); ++OtherObject)
+		{
+			if ((*CurrentObject)->testForCollision(*OtherObject) )
+			{
+				//the objects have collided
+			}
+		}
+	}
+}
+
+void Game::loadScene()
+{
+	ObjectFactory factory;
+
+	irr::scene::ISceneManager* scenemgr = Irrlicht::getDevice()->getSceneManager();
+
+	using irr::core::vector3df;
+	//Make the playership the first thing in the list of objects
+	m_Player.loadPlayerObjects();
+	m_Objects.push_back(m_Player.getShipObject() );
+
+	vector3df startpos = m_PlayArea.center();
+	startpos.X += 0.5f*m_PlayArea.right();
+	m_Player.getShipObject()->getSceneNode()->setPosition(startpos);
+	m_Player.getShipObject()->getSceneNode()->setRotation(vector3df(0, 0, 90) );
+
+
+	//Create some Asteroids
+	float asteroidspacing = 1.3f;
+	startpos = m_PlayArea.center();
+	startpos.X += 0.33f*m_PlayArea.left();
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < i+1; ++j)
+		{
+			MovingObject* newasteroid = factory.createAsteroid();
+			float spacing = 2.0f*asteroidspacing*newasteroid->getRadius();
+			vector3df placement(startpos);
+			placement.X -= i*(spacing);
+			placement.Y += j*spacing - (0.5f*i)*spacing;
+
+			newasteroid->getSceneNode()->setPosition(placement);
+			m_Objects.push_back(newasteroid);
+		}
+	}
+
+	//Create the Black Holes
+	//top-left
+	MovingObject* newBlackHole = factory.createBlackHole();
+	newBlackHole->getSceneNode()->setPosition(vector3df(m_PlayArea.left(), m_PlayArea.top(), 0) );
+	m_Objects.push_back(newBlackHole);
+	//top-right
+	newBlackHole = factory.createBlackHole();
+	newBlackHole->getSceneNode()->setPosition(vector3df(m_PlayArea.right(), m_PlayArea.top(), 0) );
+	m_Objects.push_back(newBlackHole);
+	//bottom-left
+	newBlackHole = factory.createBlackHole();
+	newBlackHole->getSceneNode()->setPosition(vector3df(m_PlayArea.left(), m_PlayArea.bottom(), 0) );
+	m_Objects.push_back(newBlackHole);
+	//bottom-right
+	newBlackHole = factory.createBlackHole();
+	newBlackHole->getSceneNode()->setPosition(vector3df(m_PlayArea.right(), m_PlayArea.bottom(), 0) );
+	m_Objects.push_back(newBlackHole);
+	//top-centre
+	newBlackHole = factory.createBlackHole();
+	newBlackHole->getSceneNode()->setPosition(vector3df(0.0f, m_PlayArea.top(), 0) );
+	m_Objects.push_back(newBlackHole);
+	//bottom-centre
+	newBlackHole = factory.createBlackHole();
+	newBlackHole->getSceneNode()->setPosition(vector3df(0.0f, m_PlayArea.bottom(), 0) );
+	m_Objects.push_back(newBlackHole);
+
+	//Add the camera
+	irr::scene::ICameraSceneNode* camera = scenemgr->addCameraSceneNode(0, vector3df(0, 0, 0),
+						vector3df(0, 0, 0) );
+	if (camera)
+	{
+		//calculate the distance to move the camera back
+		float cameradistance = m_PlayArea.top()/tanf(camera->getFOV()*0.5f );
+		camera->setPosition(vector3df(0, 0, -1.0f*cameradistance) );
+		camera->setUpVector(vector3df(0, 1, 0) );
+
+		LOG_INFO("Main Camera created and activated");
+	}
+
+	//Set up ambient lighting
+	irr::video::SColorf ambientLightColor;
+	ambientLightColor.set(0.2f, 0.2f, 0.2f);
+	scenemgr->setAmbientLight(ambientLightColor);
+	LOG_INFO("Ambient Lighting configured");
+
+
+	//Set up the main directional light
+	irr::scene::ILightSceneNode* mainlight = scenemgr->addLightSceneNode(0, vector3df(-1.0f, 1.0f, -2.0f).normalize() );
+	if (mainlight != NULL)
+	{
+		LOG_INFO("Main Directional Light added");
+		mainlight->getLightData().Type = irr::video::ELT_DIRECTIONAL;
+	}
+
+}
+
+void Game::releaseScene()
+{
+	m_Player.releasePlayerObjects();
+
+	//remove the PlayerShip from the objects list
+	m_Objects.erase(m_Objects.begin() );
+
+	//delete every asteroid in the list, then the list
+	for (Object_it i = m_Objects.begin(); i != m_Objects.end(); ++i)
+	{
+		delete (*i);
+	}
+	m_Objects.clear();
+
+	Irrlicht::getDevice()->getSceneManager()->clear();
+}
---
+++ trunk/src/MovingObject.cpp
@@ -0,0 +1,298 @@
+#include "MovingObject.h"
+
+
+MovingObject::MovingObject(irr::scene::ISceneNode* node, irr::scene::PointMassAnimator* animator)
+{
+	m_SceneNode = node;
+	m_MoveAnimator = animator;
+	m_LastCollider = 0;
+	calculateBoundingSphere();
+}
+
+MovingObject::~MovingObject(void)
+{
+	releaseObjects();
+}
+
+void MovingObject::calculateBoundingSphere()
+{
+	if (m_SceneNode == 0)
+	{
+		m_Radius = 0.0f;
+		return;
+	}
+
+	using irr::core::vector3df;
+
+	//use the Bounding box from the scene node to calculate the bounding sphere
+	const irr::core::aabbox3df& bb = m_SceneNode->getTransformedBoundingBox();
+	m_Radius = 0.5f*(bb.getExtent().X - bb.getCenter().X);
+	m_Radius = irr::core::max_(m_Radius, 0.5f*(bb.getExtent().Y - bb.getCenter().Y) );
+	//No need to check Z-extents of bounding box
+}
+
+bool MovingObject::testForCollision(MovingObject* other)
+{
+	if (!m_SceneNode || !other)
+		return false;
+
+	if (!other->getSceneNode() )
+		return false;
+
+	//no collision if either of the scene nodes are disabled
+	if (!m_SceneNode->isVisible() || !other->getSceneNode()->isVisible() )
+		return false;
+
+	/*********************************************************************************************************/
+	//Inter-penetrating collision detection code & Hacked response code
+	using irr::core::vector3df;
+	float bothradii = this->getRadius() + other->getRadius();
+	vector3df distance = other->getSceneNode()->getPosition() - this->getSceneNode()->getPosition();
+
+
+	if (distance.getLengthSQ() <= bothradii * bothradii)
+	{
+		//Objects are overlapping
+		float magnitude = 0.0f;
+
+		if (m_MoveAnimator && other->getAnimator() )
+		{
+			vector3df newvelocity(this->getAnimator()->getVelocity() );
+			newvelocity.invert();
+			newvelocity += other->getAnimator()->getVelocity();
+			magnitude = 0.5f*newvelocity.getLength();
+		}
+
+		//separate magnitude in the direction of the collision
+		vector3df collisionvector(distance);
+		float multiplier = magnitude / collisionvector.getLength();
+		collisionvector *= multiplier;
+
+		other->collide(this, collisionvector);
+		other->m_LastCollider = this;
+		this->collide(other, -collisionvector);
+		this->m_LastCollider = other;
+		return true;
+	}
+	/*********************************************************************************************************/
+
+
+	/*********************************************************************************************************
+	//Collision detection code based on "Pool Hall Lessons" by Joe van den Heuvel and Miles Jackson [18.1.02]
+	//http://www.gamasutra.com/features/20020118/vandenhuevel_02.htm
+
+	using irr::core::vector3df;
+
+	//create the relative movement vector
+	vector3df movevec(0, 0, 0);
+	
+	if (this->getAnimator() )
+		movevec += this->getAnimator()->getVelocity();
+	if (other->getAnimator() )
+		movevec -= other->getAnimator()->getVelocity();
+
+	//Early escape test, if movevec is 0 - there aint no mevement goin on
+	if (movevec.getLengthSQ() == 0.0f)
+		return false;
+
+	//Early escape test, if the length of the movevec is less than the distance
+	//between the closest edges of the circles, there's no way they can hit
+	float distsq = other->getSceneNode()->getPosition().getDistanceFromSQ(this->getSceneNode()->getPosition() );
+	float sumradii = (this->getRadius() + other->getRadius() );
+	float sumradiisq = sumradii * sumradii;
+
+	distsq -= sumradiisq;
+	if (movevec.getLengthSQ() < distsq)
+		return false;
+
+	//Normalise the movevec
+	vector3df N(movevec);
+	N.normalize();
+
+	//Find C, the vector from the centre of circleA to the centre of circleB
+	vector3df C = other->getSceneNode()->getPosition() - this->getSceneNode()->getPosition();
+
+	//D = N dot C = ||C|| * cos(angle between N and C)
+	float D = N.dotProduct(C);
+
+	//Another Early Escape test: Make sure that A is moving towards B
+	//If the dotproduct of N and C is 0, the sphere's are moving away from each other
+	if (D <= 0.0f)
+		return false;
+
+	//F = length(C)^2 - D^2
+	float F = C.getLengthSQ() - (D*D);
+
+	//Escape test: if the closest that A will get to B (sqrt(F) ) is more than the sum of their radii
+	//then they won't collide
+	if (F > sumradiisq)
+		return false;
+
+	//Find the third side of the triangle between centre of the two circles when they touch,
+	// and the closest they get to each other: sqrt(T)
+	float T = sumradiisq - F;
+
+	//If there is no right triangle with sides of length sumradii and sqrt(F), T will
+	//probably be less than zero
+	if (T < 0.0f)
+		return false;
+
+	float distance = D - sqrt(T);
+	if (distance < 0.0f)
+	{
+		//the objects have already interpenetrated
+	}
+
+	//get the magnitude of the movement vector
+	float mag = movevec.getLength();
+
+	//Make sure the distance A has to move to touch B is not greater than
+	//the magnitude of the movement vector
+	if (mag < distance)
+		return false;
+
+	//IT IS CONFIRMED THAT THE OBJECTS WILL COLLIDE!!!
+
+	//calculate the multiplier of each object's original velocity magnitude
+	float thismult = 0.0f;
+	float othermult = 0.0f;
+
+	//Only move them out of penetration if both objects can't be penetrated (i.e. have PointMassAnimators)
+	if (other->getAnimator() && this->getAnimator() )
+	{
+		if (this->getAnimator()->getSpeed() != 0.0f)
+			thismult = distance / this->getAnimator()->getSpeed();
+		if (other->getAnimator()->getSpeed() != 0.0f)
+			othermult = distance / other->getAnimator()->getSpeed();
+
+		//move the objects to stop them interpenetrating
+		//this->getSceneNode()->setPosition(this->getSceneNode()->getPosition() 
+		//					+ thismult*this->getAnimator()->getVelocity() );
+		//other->getSceneNode()->setPosition(other->getSceneNode()->getPosition() 
+		//					+ othermult*other->getAnimator()->getVelocity() );
+		this->getAnimator()->setVelocity(this->getAnimator()->getVelocity() + thismult*this->getAnimator()->getVelocity() );
+		other->getAnimator()->setVelocity(othermult*other->getAnimator()->getVelocity() );
+	}
+	
+	//calculate the collision normal
+	vector3df norm(0, 1, 0);// = other->getSceneNode()->getPosition() - this->getSceneNode()->getPosition();
+
+	//this->collide(other, norm);
+	//this->m_LastCollider = other;
+	//other->collide(this, norm);
+	//other->m_LastCollider = this;
+	*********************************************************************************************************/
+
+	return true;
+}
+
+void MovingObject::projectOutOfCollision(const MovingObject* collider)
+{
+	using irr::core::vector3df;
+	if (!m_SceneNode || !m_MoveAnimator || !collider)
+		return;
+
+	if (!collider->getAnimator() || !collider->getSceneNode() )
+		return;
+
+
+
+	//calculate distance to move the spheres back
+	float bothrads = m_Radius + collider->getRadius();
+	float dist = static_cast<irr::f32>(m_SceneNode->getPosition().getDistanceFrom(
+				collider->getSceneNode()->getPosition() ) );
+	dist = bothrads - dist;
+	//Old code here:
+	//float distsq = bothrads + m_SceneNode->getPosition().getDistanceFromSQ(
+	//			collider->getSceneNode()->getPosition() );
+	//float dist = sqrtf(distsq) - bothrads;
+
+	//create the total change of position (velocity) vector
+	vector3df vtotal(m_MoveAnimator->getVelocity() );
+	vtotal -= collider->getAnimator()->getVelocity();
+	if (vtotal != vector3df(0, 0, 0) )
+	{
+		vtotal.setLength(dist);
+	}
+
+	//split the total change into this object's part
+	vector3df movevelocity(m_MoveAnimator->getVelocity() );
+	float distance = 0.0f;
+	if (movevelocity.getLengthSQ() != 0.0f )
+	{
+		//calculate how much of the change of position needs to be done by this object
+		movevelocity.normalize();
+		distance = vtotal.dotProduct(movevelocity);
+
+		//calculate the new velocity
+		movevelocity *= distance;
+	}
+	//edit this object's position
+	m_SceneNode->setPosition(m_SceneNode->getPosition() + movevelocity);
+
+	//DEBUG: edit this node's velocity
+	m_MoveAnimator->addVelocity(vtotal/2.0f);
+}
+
+void MovingObject::releaseObjects()
+{
+	if (m_SceneNode)
+	{
+		m_SceneNode->remove();
+		m_SceneNode = 0;
+	}
+
+	if (m_MoveAnimator)
+	{
+		m_MoveAnimator->drop();
+		m_MoveAnimator = 0;
+	}
+}
+
+void MovingObject::elasticCollision(const MovingObject* other, const irr::core::vector3df& collisionnormal,
+		const irr::core::vector3df& collisionpoint)
+{
+	//calculate the new position for the node
+	using irr::core::vector3df;
+	vector3df newpos = collisionpoint - this->getRadius() * -collisionnormal;
+
+	this->getSceneNode()->setPosition(newpos);
+
+
+	//find the length of the component of each of the velocity vectors along the normal
+	float a1 = this->getAnimator()->getVelocity().dotProduct(collisionnormal);
+	float a2 = other->getAnimator()->getVelocity().dotProduct(collisionnormal);
+
+	//P = 2(a1 - a2)
+	//    ----------
+	//      m1 + m2
+	float P = (2.0f * (a1 - a2) ) / (this->getAnimator()->getMass() + 
+		other->getAnimator()->getMass() );
+
+	//calculate v', the new movement vector
+	//	v' = v - P * m1 * n
+	vector3df vdash = this->getAnimator()->getVelocity() + P * 
+					other->getAnimator()->getMass() * collisionnormal;
+
+	this->getAnimator()->setVelocity(-vdash);
+}
+
+float MovingObject::calcImpulse(const MovingObject *other,  const irr::core::vector3df &normal, float restitution)
+{
+	if (!other)
+		return 0.0f;
+
+	if (!this->getAnimator() || !other->getAnimator() )
+		return 0.0f;
+
+	//create the combined velocity vector
+	irr::core::vector3df velAB = this->getAnimator()->getVelocity() - other->getAnimator()->getVelocity();
+
+	// j =  -(1 + E)Vab.n
+	//     ----------------
+	//     n.n(1/Ma + 1/Mb)
+	float top = -(1.0f + restitution) * velAB.dotProduct(normal);
+	float bottom = normal.dotProduct(normal) * (1/this->getAnimator()->getMass() + 1/other->getAnimator()->getMass() );
+
+	return top/bottom;
+}
