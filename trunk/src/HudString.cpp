485fccfdc91f196fd58b5c4c8edb94c9d6d5e720
tjohns20 <tjohns20@2d35bbc6-a434-0410-ba66-8ba39d4a960b>
2010-10-08 18:44:46 +0000

    using large and small fonts in HudString

--- trunk/src/HudString.cpp
+++ trunk/src/HudString.cpp
@@ -20,13 +20,15 @@ using namespace irr;
 using namespace irr::core;
 
 HudString::HudString(void)
-: m_TextControl(0)
+: m_TextControl(0),
+m_FontType(HudFont::Small)
 {
 	SetText("");
 }
 
 HudString::HudString(const std::string& str)
-: m_TextControl(0)
+: m_TextControl(0),
+m_FontType(HudFont::Small)
 {
 	SetText(str);
 }
@@ -44,15 +46,67 @@ void HudString::SetText(const std::string &str)
 	std::wstring wideText(m_Text.length(), L' ');
 	std::copy(m_Text.begin(), m_Text.end(), wideText.begin() );
 
+	dimension2di clipSize;
+	if (m_TextControl && m_TextControl->getOverrideFont() )
+	{
+		clipSize = m_TextControl->getOverrideFont()->getDimension(wideText.c_str() );
+	}
+	else
+	{
+		clipSize = Irrlicht::getDevice()->getGUIEnvironment()->getBuiltInFont()->getDimension(wideText.c_str() );
+		clipSize *= 2;	//the values returned by getDimension are too small, hack to double them
+	}
+
+	//Here be dragons: having to continuously recreate the text causes fickering
 	releaseSprite();
-	dimension2di clipSize =  Irrlicht::getDevice()->getGUIEnvironment()->getBuiltInFont()->getDimension(wideText.c_str() );
-	clipSize *= 2;	//the values returned by getDimension are too small, hack to double them
 	m_TextControl =  Irrlicht::getDevice()->getGUIEnvironment()->addStaticText(
 		wideText.c_str(), rect<s32>(position2di(), clipSize) );
-	m_TextControl->setOverrideColor(irr::video::SColor(255, 255, 255, 255) );
+
+	//have to refresh the override font because the text control has been recreated
+	SetFont(m_FontType);
+}
+
+void HudString::SetFont(HudFont::Enum hudFont)
+{
+	m_FontType = hudFont;
+	switch (m_FontType)
+	{
+	case HudFont::Large:
+		m_TextControl->setOverrideFont(HudString::getLargeFont() );
+		break;
+
+	case HudFont::Small:
+		m_TextControl->setOverrideFont(HudString::getSmallFont() );
+		break;
+
+	default:
+		m_TextControl->setOverrideFont(0);
+		break;
+	}
 }
 
 void HudString::releaseSprite()
 {
 	if (m_TextControl) { m_TextControl->remove(); }
+}
+
+/**Hacky resource management**/
+irr::gui::IGUIFont* HudString::getLargeFont()
+{
+	static irr::gui::IGUIFont* largeFont = 0;
+	if (0 == largeFont)
+	{
+		largeFont = Irrlicht::getDevice()->getGUIEnvironment()->getFont("fontLarge.xml");
+	}
+	return largeFont;
+}
+
+irr::gui::IGUIFont* HudString::getSmallFont()
+{
+	static irr::gui::IGUIFont* smallFont = 0;
+	if (0 == smallFont)
+	{
+		smallFont = Irrlicht::getDevice()->getGUIEnvironment()->getFont("fontSmall.xml");
+	}
+	return smallFont;
 }
