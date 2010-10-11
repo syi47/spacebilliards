117e8d39f40eb68b9122a0f067e73745fdffde96
tjohns20 <tjohns20@2d35bbc6-a434-0410-ba66-8ba39d4a960b>
2010-10-11 21:41:47 +0000

    replaced T.Phillip in comments with Tatham

--- trunk/src/HudString.h
+++ trunk/src/HudString.h
@@ -1,4 +1,4 @@
-/*   Copyright 2010 T. Phillip Johnson
+/*   Copyright 2010 Tatham Johnson
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
