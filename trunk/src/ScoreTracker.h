5aad8151c76cd9b8e576e267be492de2ea1cb379
tjohns20 <tjohns20@2d35bbc6-a434-0410-ba66-8ba39d4a960b>
2010-10-16 23:08:42 +0000

    documented ScoreTracker

--- trunk/src/ScoreTracker.h
+++ trunk/src/ScoreTracker.h
@@ -22,9 +22,9 @@ class Score
 public:
 	Score(int time, const std::string& name)
 		: m_Time(time), m_Name(name) {}
-	int Time() const { return m_Time; }
-	const std::string& Name() const { return m_Name; }
-	bool operator<(const Score& rvalue) const { return m_Time < rvalue.Time(); }
+	int Time() const { return m_Time; }	///< The time of the score
+	const std::string& Name() const { return m_Name; }	///< The name of the player who owns the score
+	bool operator<(const Score& rvalue) const { return m_Time < rvalue.Time(); }	///< Used to sort the scores
 private:
 	int m_Time;
 	std::string m_Name;
@@ -34,21 +34,44 @@ private:
 class ScoreTracker
 {
 public:
+	///Constructor
+	/** @param fileName The location of the file to save and load scores to/from
+	**/
 	ScoreTracker(const std::string& fileName);
+	///Destructor
 	~ScoreTracker(void);
 
+	///Saves the scores to the file
 	void save();
+	///Loads the scores from the file
 	void load();
 
-	int addScore(int time, const std::string& name);
+	///Adds a score to the current scores.
+	/** Note: does not automatically save the scores
+	**/
+	void addScore(int time, const std::string& name);
+
+	///Checks if a score would make the High Scores
+	/** @return true if the score is a top score, false otherwise
+	**/
+	bool isHighScore(int timeInMilliseconds);
+	///Gets the number of scores currently stored
+	/** @return The number of scores
+	**/
 	unsigned int count() const {return m_Scores.size(); }
+	///Clears all the high scores
+	/** This will clear all the scores stored inside the file as well
+	**/
 	void clearScores();
 
+	///Gets the score at the given index
 	const Score& at(int index) const;
+	///Gets the score at the given index, using square bracket operators
 	const Score& operator[](int index) const { return at(index); }
 
 private:
 private:
+	///Sorts the scores lowest (best) to highest (worst)
 	void sortScores();
 
 private:
